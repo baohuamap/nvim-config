a2225dadad15cab2a2d2a821d8801339cc85eda4
bhmap <68145237+baohuamap@users.noreply.github.com>
2024-05-20 21:39:04 +0700

    update nvim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,9 +15,7 @@ require("lazy").setup({
       colorscheme = "solarized-osaka",
     } },
     -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    --
     -- import/override with your plugins
     { import = "plugins" },
   },
