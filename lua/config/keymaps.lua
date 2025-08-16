9ba8415f7ee19746dc205ff3326a86c83262bae4
bhmap <baohua3110@gmail.com>
2025-08-16 12:53:09 +0700

    clean some config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,11 +14,11 @@ keymap.set("n", "ss", ":split<Return>", opts)
 keymap.set("n", "sv", ":vsplit<Return>", opts)
 
 -- Move window
-keymap.set("n", "sh", "<C-w>h")
-keymap.set("n", "sk", "<C-w>k")
-keymap.set("n", "sj", "<C-w>j")
-keymap.set("n", "sl", "<C-w>l")
-
+-- keymap.set("n", "sh", "<C-w>h")
+-- keymap.set("n", "sk", "<C-w>k")
+-- keymap.set("n", "sj", "<C-w>j")
+-- keymap.set("n", "sl", "<C-w>l")
+--
 -- Resize window
 keymap.set("n", "<C-w><left>", "<C-w><")
 keymap.set("n", "<C-w><right>", "<C-w>>")
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,28 +1,3 @@
--- return {
---   {
---     "Mofiqul/dracula.nvim",
---     lazy = true,
---     priority = 1000,
---     opts = function()
---       return {
---         transparent_bg = true,
---       }
---     end,
---   },
--- }
-
---return {
---  {
---    "craftzdog/solarized-osaka.nvim",
---    lazy = true,
---    priority = 1000,
---    opts = function()
---      return {
---        transparent = true,
---      }
---    end,
---  },
---}
 return {
   {
     "catppuccin",
